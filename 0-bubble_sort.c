e4b16b1cc0d1d75a13c4b54b7926016c42362b42
tluciad <tluciad@gmail.com>
2022-06-17 19:11:14 -0500

    bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -20,7 +20,7 @@ void bubble_sort(int *array, size_t size)
 		{
 			if (array[i] > array[i + 1])
 			{
-				array[i] += array[i + 1];
+				array[i] = array[i] + array[i + 1];
 				array[i + 1] = array[i] - array[i + 1];
 				array[i] = array[i] - array[i + 1];
 				print_array(array, size);
